7e46ce67782e89c8fa8d96e01cec1d301cf01137
alexSam <alexsamalot19@gmail.com>
2020-02-14 08:28:44 -0500

    return form validation and automatic page refresh/form close

--- src/App.js
+++ src/App.js
@@ -1,26 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Container from "./Container";
+import Footer from "./Footer";
+import { render } from "@testing-library/react";
+import { getAllStudents } from "./client.js";
+import { Table, Avatar, Spin, Icon, Modal } from "antd";
+import AddStudentForm from "./forms/AddStudentForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    students: [],
+    isFetching: false,
+    isAddStudentModalVisible: false
+  };
+
+  componentDidMount() {
+    this.fetchStudents();
+  }
+
+  openAddStudentModal = () => this.setState({ isAddStudentModalVisible: true });
+  closeAddStudentModal = () =>
+    this.setState({ isAddStudentModalVisible: false });
+
+  fetchStudents = () => {
+    this.setState({
+      isFetching: true
+    });
+    getAllStudents().then(res =>
+      res.json().then(students => {
+        console.log(students);
+        this.setState({
+          students,
+          isFetching: false
+        });
+      })
+    );
+  };
+
+  render() {
+    const { students, isFetching, isAddStudentModalVisible } = this.state;
+
+    if (isFetching) {
+      return (
+        <Container>
+          <Spin />
+        </Container>
+      );
+    }
+
+    if (students && students.length) {
+      const columns = [
+        {
+          title: "",
+          dataIndex: "avatar",
+          render: (text, student) => (
+            <Avatar size="large">
+              {`${student.firstName
+                .charAt(0)
+                .toUpperCase()}${student.lastName.charAt(0).toUpperCase()}`}
+            </Avatar>
+          )
+        },
+        {
+          title: "Student Id",
+          dataIndex: "studentId",
+          key: "studentId"
+        },
+        {
+          title: "First Name",
+          dataIndex: "firstName",
+          key: "firstName"
+        },
+        {
+          title: "Last Name",
+          dataIndex: "lastName",
+          key: "lastName"
+        },
+        {
+          title: "Email",
+          dataIndex: "email",
+          key: "email"
+        },
+        {
+          title: "Gender",
+          dataIndex: "gender",
+          key: "gender"
+        }
+      ];
+
+      return (
+        <Container>
+          <Table
+            style={{ marginBottom: "100px" }}
+            dataSource={students}
+            columns={columns}
+            pagination={false}
+            rowKey="studentID"
+          />
+          <Modal
+            title="Add new student"
+            visible={isAddStudentModalVisible}
+            onOk={this.closeAddStudentModal}
+            onCancel={this.closeAddStudentModal}
+            width={1000}
+          >
+            <AddStudentForm
+              onSuccess={() => {
+                this.closeAddStudentModal();
+                this.fetchStudents();
+              }}
+            />
+          </Modal>
+          <Footer
+            handleAddStudentClickEvent={this.openAddStudentModal}
+            numberOfStudents={students.length}
+          ></Footer>
+        </Container>
+      );
+    }
+
+    return <h1>No Student Found</h1>;
+  }
 }
 
 export default App;
